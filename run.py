44e9991354a5443dc379c9e1de9e79081fa3d27f
Artem <poznyshev.a.a@yandex.ru>
2024-05-09 14:15:14 +0400

    init_bot

--- run.py
+++ run.py
@@ -1,20 +1,35 @@
+"""
+Чат бот телеграмм
+
+ЗадаЧа:
+
+1. Приветственные сообщения (сразу без лишней воды:
+
+"""
 
 import asyncio
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart  # Фильтор только для старта
 
 from config_bot import *
 
-
-
+# ----------------------------------------------------------------------------------------------------------------------
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
 
+
+
+
+# ------------------------ Тело
+# Реакция на старт бота:
 @dp.message(CommandStart())
 async def start_cmd(message: types.Message):
-    await message.answer('Это была команда старт')
+    user = message.from_user.first_name  # Имя поальзователя
+    await message.answer(f'Привет, {user}  на связи "Tasks bot"! \n'
+                         f'Давай попробуем решить твой вопрос.')
 
+# Ответ на вариации входящих сообщений:
 @dp.message()
 async def echo(message: types.Message):
     text = message.text
@@ -26,11 +41,16 @@ async def echo(message: types.Message):
     else:
         await message.answer(message.text)
 
+
+
+
+
+
 # ------------------------ Зацикливание работы бота
 # Отслеживание событий на сервере тг бота:
 async def run_bot():
     await dp.start_polling(bot)
 
+
 # Запуск асинхронной функции run_bot:
 asyncio.run(run_bot())
-
