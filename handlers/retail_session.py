00be346c3e7b042b166d01b2ca9a507f183308c0
Artem <poznyshev.a.a@yandex.ru>
2024-05-19 10:22:29 +0400

    query builder

--- handlers/retail_session.py
+++ handlers/retail_session.py
@@ -11,6 +11,8 @@ from aiogram import types, Router, F
 from aiogram.filters import CommandStart, Command, StateFilter, or_f
 from aiogram.client.default import DefaultBotProperties  # Обработка текста HTML разметкой
 
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
 # -------------------------------- Локальные модули
 from handlers.text_message import *  # Список ругательств:
 from filters.chats_filters import *
@@ -34,8 +36,8 @@ retail_router.message.filter(ChatTypeFilter(['private']), UsersRetailSession())
 retail_router.edited_message.filter(ChatTypeFilter(['private']), UsersRetailSession())
 
 # ----------------------------------------------------------------------------------------------------------------------
-# Кнопки меню внизу (более менее нормально, потом доработать)
-RETAIL_KEYB = get_keyboard(
+# Кнопки меню внизу (первый старт)
+RETAIL_KEYB_MAIN = get_keyboard(
     'Создать заявку',
     'Изменить заявку',
     'Удалить заявку',
@@ -44,6 +46,28 @@ RETAIL_KEYB = get_keyboard(
     placeholder='Выберите действие',
     sizes=(2, 1, 1)  # кнопок в ряду, по порядку 1й ряд и тд.
 )
+
+REQUEST_PROBLEM = get_keyboard(
+    # 'Изменить категорию',
+    'Отменить заявку',
+    'Показать справку по категориям',
+
+    placeholder='Выберите действие',
+    sizes=(1, 1)  # кнопок в ряду, по порядку 1й ряд и тд. 2, 1
+)
+
+
+# Код ниже для машины состояний (FSM)
+class AddRequests(StatesGroup):
+    # Шаги состояний
+    request_message = State()
+    # documents = State()
+
+
+texts = {
+    'AddRequests:request_message': 'Введите текст обращения заново:'
+    # + ЕЩЕ
+}
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -63,7 +87,8 @@ async def hello_after_on_next(message: types.Message):
 
     await asyncio.sleep(4)
     await message.answer(f'Я сейчас включил специальное меню внизу экрана.\n'
-                         f'С помощью него ты сможешь взаимодействовать с моим функционалом.', reply_markup=RETAIL_KEYB)
+                         f'С помощью него ты сможешь взаимодействовать с моим функционалом.'
+                         , reply_markup=RETAIL_KEYB_MAIN)
 
     await asyncio.sleep(6)
     await message.answer(f'Для того, чтобы обратиться в ОАиТ за помощью в решении проблем '
@@ -89,19 +114,86 @@ async def hello_after_on_next(message: types.Message):
     await message.answer(f'Если что, - я готов! Жалуйся ✍️ !')
 
 
+# ----------------------------------------------------
+# 1. первая кнопка в меню: сценарий 1)
+@retail_router.message(F.text == 'Создать заявку')
+async def get_request_problem(message: types.Message):
+    # удалит сообщение 'Создать заявку' - отправляется в чат после нажатия клавиатуры (не изменяемое поведение)
+    await message.delete()
+
+    # Заменяет старое меню на новое
+    await message.answer(f'Задачу понял!', reply_markup=REQUEST_PROBLEM)
+
+    await asyncio.sleep(1)
+    await message.answer(f'Выберите категорию обращения',
+                         reply_markup=inline_menu.get_callback_btns(
+                             btns={'АНАЛИТИКА': 'analytics',
+                                   'ФОРМАТЫ': 'formats',
+                                   'ТОВАРООБОРОТ': 'trade _turnover'},
+                             sizes=(1, 1, 1)
+                         )
+                         )
+
+
+# ---------------------------- Келбек квери:
+
+@retail_router.callback_query(StateFilter(None), F.data.startswith('analytics'))
+# Если у пользователя нет активного состояния (StateFilter(None) + он ввел команду "analytics")
+async def add_request_message(callback: types.CallbackQuery, state: FSMContext):
+    await callback.answer()
+    await callback.message.answer('Введите текст обращения', reply_markup=types.ReplyKeyboardRemove())
+    await state.set_state(AddRequests.request_message)
+    # todo замутить удаление всех сообщений с меню
+
+
+# Становимся в состояние ожидания ввода
+# Пользователь ввел текст
+@retail_router.callback_query(AddRequests.request_message, F.text)
+# Если ввел текст обращения (AddRequests.request_message, F.text):
+async def get_request_message(message: types.Message, state: FSMContext):
+    # Передам словарь с данными ( ключ = request_message, к нему присваиваем данные message.text), после апдейтим
+    await state.update_data(request_message=message.text)
+
+    await message.answer('Обращение отправлено. Ожидайте ответа. \n'
+                         'Я направлю уведомление, как только обращение будет взято в работу.'
+                         , reply_markup=RETAIL_KEYB_MAIN)
+
+    # Формируем полученные данные:
+    data = await state.get_data()
+
+    # Отправка для наглядности записанного сообщения:
+    await message.answer(text=f'Ваша жалоба: {str(data)}')
+
+    # Очистка состояния пользователя:
+    await state.clear()
 
 
 
 
 
 
+# ---------------------------- Нижняя сопутствующая клавиатура кнопке "Создать заявку":
+# back_step
+@retail_router.message(F.text == 'Отменить заявку')
+async def get_back_request(message: types.Message):
+    await message.delete()
+
+    # Заменяет старое меню на новое
+    await message.answer(f'Ок!', reply_markup=RETAIL_KEYB_MAIN)
 
 
+@retail_router.message(F.text == 'Показать справку по категориям')
+async def get_info_category(message: types.Message):
+    await message.delete()
+
+    await message.answer(category_problem, parse_mode='HTML')
+
+# ---------------------------- Нижняя сопутствующая клавиатура кнопке "Создать заявку" конец:
 
 
+# await message.answer(f'Введите суть обращения', reply_markup=REQUEST_PROBLEM)
 
-
-
+# , reply_markup=types.ReplyKeyboardRemove() - удаляет клаву # Удалить старое меню
 
 # f'А если вдруг тебе понадобится '
 #     await asyncio.sleep(1)
@@ -135,7 +227,6 @@ async def hello_after_on_next(message: types.Message):
 #                      }))  # create
 #                     # сделать друг на друга кнопки#
 
-
 # Реакция на нажатие кнопки Новая заявка: (or_f(Command("menu"), (F.text.lower() == "меню")))
 # @retail_router.callback_query(F.data.startswith('new'))
 # async def callback_new(callback: types.CallbackQuery): # для бд -   , session: AsyncSession
@@ -146,11 +237,9 @@ async def hello_after_on_next(message: types.Message):
 #     await callback.answer()  # для сервера ответ
 #     await callback.message.answer(category_problem, parse_mode='HTML') #.as_html() - похоже не работает с f строкой
 
-
 # @retail_router.message(CommandStart())
 # async def start_cmd(message: types.Message):
 #     await message.answer("Привет, я виртуальный помощник")
-
 
 # , parse_mode='HTML', reply_markup=inline_menu.get_callback_btns(btns={async def add_product(message: types.Message):
 #                                      'Пройти атентификацию': 'get_type_users'}))   # , parse_mode='HTML'    await message.answer("Привет розница! Что хочешь сделать?", reply_markup=RETAIL_KEYB)
